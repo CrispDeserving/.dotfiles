f077e03787f40b84671c5b880e5e3c08620e43aa
Jabez Villamarzo <math2btrue@gmail.com>
2023-11-19 10:01:55 +0800

    Make i3lockr_script independent of user

--- deploy-config.sh
+++ deploy-config.sh
@@ -2,5 +2,12 @@
 set -e
 
 script_path="$(dirname $0)"
+script_name="deploy-config"
 
 cp -r -u "$script_path/.config" $HOME
+
+i3lockr_path="$HOME/scripts/i3/i3lockr"
+if [[ ! -e $i3lockr_path ]]; then
+	echo "($script_name) **Warning**: i3lockr is required in this path: $i3lockr_path"
+fi
+
