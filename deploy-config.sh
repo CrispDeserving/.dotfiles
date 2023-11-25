71098a1aba33ae33d52fec182e775d8d04e31d8d
Jabez Villamarzo <simplesimmering@gmail.com>
2023-11-25 20:55:38 +0800

    Add notification for dunst testing in deploy-script

--- deploy-config.sh
+++ deploy-config.sh
@@ -9,10 +9,11 @@ if [[ ! -e $i3lockr_path ]]; then
 	echo "($script_name) **Warning**: i3lockr is required in this path: $i3lockr_path"
 fi
 
+rsync -a $script_path/config/ $HOME/.config
 if [[ $# -ne 0 ]]; then
 	sudo cp -r -u "$script_path/keyd" /etc 
 	sudo keyd reload
 fi
 
-rsync -a $script_path/config/ $HOME/.config
-killall dunst
+killall dunst || error_code=$? # prevents exit on error
+notify-send --urgency=low --expire-time=5000 "Config file updated" "Try to see what changed!"
