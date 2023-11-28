461cdb6d11213e1f8e9eb7ee8dc9b17b439447f5
Jabez Villamarzo <math2btrue@gmail.com>
2023-11-28 15:01:07 +0800

    refactor(deploy-script): Add help echo, better sudo flags
    Also exit when invalid args.

--- deploy-config.sh
+++ deploy-config.sh
@@ -4,16 +4,40 @@ set -e
 script_path="$(dirname $0)"
 script_name="deploy-config"
 
+show_help() {
+	echo "
+	$script_name
+	
+	Usage:
+		-h/--help: Show this help.
+		-s/--sudo: Execute commands that needs sudo permissions.
+	"
+}
+
+if [[ $# -ne 0 ]]; then
+	case $1 in
+		-s)
+			sudo cp -r -u "$script_path/keyd" /etc 
+			sudo keyd reload
+		;;
+
+		--help|-h)
+			show_help
+		exit 0;;
+
+		*)
+			show_help
+			echo "($script_name): **Warning**: Invalid args."
+		exit 1;;
+	esac
+fi
+
 i3lockr_path="$HOME/scripts/i3/i3lockr"
 if [[ ! -e $i3lockr_path ]]; then
 	echo "($script_name) **Warning**: i3lockr is required in this path: $i3lockr_path"
 fi
 
 rsync -a $script_path/config/ $HOME/.config
-if [[ $# -ne 0 ]]; then
-	sudo cp -r -u "$script_path/keyd" /etc 
-	sudo keyd reload
-fi
 
 killall dunst || error_code=$? # prevents exit on error
 notify-send --urgency=low --expire-time=5000 "Config file updated" "Try to see what changed!"
