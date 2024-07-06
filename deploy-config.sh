c6eb053bb79573ca30cceaf11f9c2d80b24d229e
Jabez Villamarzo <simplesimmering@gmail.com>
2024-07-06 22:34:45 +0800

    Change i3lockr path

--- deploy-config.sh
+++ deploy-config.sh
@@ -37,8 +37,8 @@ if [[ $# -ne 0 ]]; then
 	esac
 fi
 
-i3lockr_path="$HOME/scripts/i3/i3lockr"
-if [[ ! -e $i3lockr_path ]]; then
+i3lockr_path="/usr/bin/i3lockr"
+if [[ ! (-e $i3lockr_path) ]]; then
 	echo "($script_name) **Warning**: i3lockr is required in this path: $i3lockr_path"
 fi
 
